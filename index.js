7c332d26faa90d0ff963a05568a0c6084912703e
Kharmalina Tong <kharmalinat7@gmail.com>
2023-01-19 15:09:32 -0500

    add logic to filter jokes by tags and content - 3 tests successfully passed

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const { Joke } = require('./db');
+const {Op} = require("sequelize");
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -8,7 +9,21 @@ app.use(express.urlencoded({extended:true}));
 app.get('/jokes', async (req, res, next) => {
   try {
     // TODO - filter the jokes by tags and content
-    const jokes = [];
+    const where = {};
+    if(req.query.tags){
+      where.tags = {
+        [Op.like]: `%${req.query.tags}%`
+      }
+    };
+    if(req.query.content){
+      where.joke = {
+        [Op.like]: `%${req.query.content}%`
+      }
+    }
+    const {tags, content} = req.query;
+    const jokes = await Joke.findAll({
+      where
+    })
     res.send(jokes);
   } catch (error) {
     console.error(error);
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const app = require('./index');
 const { sequelize } = require('./db');
 
-const { PORT = 4000 } = process.env;
+const { PORT = 4001 } = process.env;
 
 app.listen(PORT, () => {
   sequelize.sync({ force: false });
-  console.log(`Jokes are ready at http://localhost:${PORT}`);
+  console.log(`Jokes are ready at http://localhost:${PORT}/jokes`);
 });
